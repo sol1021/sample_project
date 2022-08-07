a04d1d66ba9ebbf7fb5df68d4af9f83e48c8ee53
sol <soool1021@gmail.com>
2022-08-07 21:07:04 +0900

    카카오 지도 핀 수정

---
+++ src/main/java/com/example/test/menu/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.example.test.menu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String todo(Model model){
+        model.addAttribute("page","로그인 페이지");
+        return "login";
+    }
+}
+	
+	
--- src/main/resources/static/js/kakao_map.js
+++ src/main/resources/static/js/kakao_map.js
@@ -1,31 +1,35 @@
+var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
 var container = document.getElementById("map");
 var options = {
   //지도를 생성할 때 필요한 기본 옵션
   center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-  level: 10, //지도의 레벨(확대, 축소 정도)
+  level: 12, //지도의 레벨(확대, 축소 정도)
 };
+const addr = document.querySelector(".address");
 
 var map = new kakao.maps.Map(container, options);
+/*
+// 지도를 클릭한 위치에 표출할 마커입니다
+var marker = new kakao.maps.Marker({
+  // 지도 중심좌표에 마커를 생성합니다
+  position: map.getCenter(),
+});
+// 지도에 마커를 표시합니다
+marker.setMap(map);
 
-//좌표를 주소로 출력
-let lat = 37.5566803113882;
-let lng = 126.904501286522;
-getAddr(lat, lng);
-function getAddr(lat, lng) {
-  let geocoder = new kakao.maps.services.Geocoder();
+// 지도에 클릭 이벤트를 등록합니다
+// 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+kakao.maps.event.addListener(map, "click", function (mouseEvent) {
+  // 클릭한 위도, 경도 정보를 가져옵니다
+  var latlng = mouseEvent.latLng;
 
-  let coord = new kakao.maps.LatLng(lat, lng);
-  let callback = function (result, status) {
-    if (status === kakao.maps.services.Status.OK) {
-      console.log(result[0].road_address.address_name);
-    }
-  };
-  geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-}
+  // 마커 위치를 클릭한 위치로 옮깁니다
+  marker.setPosition(latlng);
+});
 
 var geocoder = new kakao.maps.services.Geocoder();
 
-// 주소로 좌표를 검색합니다
+ 주소로 좌표를 검색합니다
 geocoder.addressSearch("제주특별자치도 제주시 첨단로 242", function (result, status) {
   // 정상적으로 검색이 완료됐으면
   if (status === kakao.maps.services.Status.OK) {
@@ -46,4 +50,51 @@ geocoder.addressSearch("제주특별자치도 제주시 첨단로 242", function
     // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
     map.setCenter(coords);
   }
-});
+});*/
+
+// 장소 검색 객체를 생성합니다
+
+var ps = new kakao.maps.services.Places();
+
+// 키워드로 장소를 검색합니다
+ps.keywordSearch("제주도 둘레길", placesSearchCB);
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB(data, status, pagination) {
+  if (status === kakao.maps.services.Status.OK) {
+    // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+    // LatLngBounds 객체에 좌표를 추가합니다
+    var bounds = new kakao.maps.LatLngBounds();
+
+    for (var i = 0; i < data.length; i++) {
+      displayMarker(data[i]);
+      bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+    }
+
+    // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+    map.setBounds(bounds);
+  }
+}
+
+var imageSrc = "../img/pin.png", // 마커이미지의 주소입니다
+  imageSize = new kakao.maps.Size(40, 40); // 마커이미지의 크기입니다
+
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+  // 마커를 생성하고 지도에 표시합니다
+  var marker = new kakao.maps.Marker({
+    map: map,
+    position: new kakao.maps.LatLng(place.y, place.x),
+    image: markerImage,
+  });
+
+  // 마커에 클릭이벤트를 등록합니다
+  kakao.maps.event.addListener(marker, "click", function () {
+    // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+    infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + "</div>");
+    infowindow.open(map, marker);
+    addr.innerText = place.place_name + "y = " + place.y + "x = " + place.x;
+  });
+}
